b6df69482fb303209b1b5051e36074e05c8b022b
Pablo Santos <pablo.roldao.santos@gmail.com>
2023-02-20 08:40:12 -0300

    Configuring vercel...

--- api/index.js
+++ api/index.js
@@ -42,10 +42,4 @@ mongoose.connect(mongoAtlasUri)
         console.log(err)
     });
 
-export default function handler(request, response) {
-    response.status(200).json({
-        body: request.body,
-        query: request.query,
-        cookies: request.cookies,
-    });
-}
+module.exports = app;
