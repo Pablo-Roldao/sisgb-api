abc088308426db05f6578101180f4c094febcf72
Pablo Santos <pablo.roldao.santos@gmail.com>
2023-02-23 21:02:50 -0300

    Fixing login function...

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,16 +2,18 @@ const User = require('../model/User');
 const bcrypt = require('bcrypt');
 
 const handleLogin = async (req, res) => {
-    const {cpf, pwd} = req.body;
-    if (!cpf || !pwd) return res.status(400).json({ 'message': 'Username and password are required.'});
+    const { cpf, password } = req.body;
+    if (!cpf || !password) {
+        return res.status(400).json({ 'message': 'CPF and password are required.' });
+    }
     const foundUser = await User.findOne({ "cpf": cpf });
     if (!foundUser) return res.sendStatus(401);
 
-    const match = await bcrypt.compare(pwd, foundUser.password);
+    const match = await bcrypt.compare(password, foundUser.password);
     if (match) {
         //create JWTs 
         res.json({
-            'message': `User ${user} is logged in!`
+            'message': `User ${cpf} is logged in!`
         })
     } else {
         res.sendStatus(401);
