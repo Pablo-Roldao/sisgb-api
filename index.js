0ad79f66c419acf0fe87db736f9e408de29398e9
Pablo Santos <pablo.roldao.santos@gmail.com>
2023-02-26 13:47:52 -0300

    Adding credentials...

--- index.js
+++ index.js
@@ -6,16 +6,22 @@ const corsOptions = require('./config/corsOptions');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
+const credentials = require('./middleware/credentials')
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 
 app.use(logger);
 
+app.use(credentials);
+
 app.use(cors(corsOptions));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(cookieParser());
 
 
 app.use("/", require("./routes/root"));
