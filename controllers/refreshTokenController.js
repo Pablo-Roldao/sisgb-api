0de3e65bd5999d994e76b57e59733fcdd68682db
Pablo Santos <pablo.roldao.santos@gmail.com>
2023-02-26 12:35:39 -0300

    Fixing refreshTokenController...

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -2,24 +2,24 @@ const User = require('../model/User');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const handleRefreshToken = (req, res) => {
+const handleRefreshToken = async (req, res) => {
     const cookies = req.cookies;
     if (!cookies?.jwt) return res.sendStatus(401);
     const refreshToken = cookies.jwt;
 
-    const foundUser = User.findOne({ "refreshToken": refreshToken });
+    const foundUser = await User.findOne({ "refreshToken": refreshToken });
     if (!foundUser) {
-        return res.sendStatus(403)
+        return res.status(404).json({"erorr": "User with this refreshToken not found!"});
     };
 
     jwt.verify(
         refreshToken,
         process.env.REFRESH_TOKEN_SECRET,
         (err, decoded) => {
-            if (err || foundUser.cpf != decoded.cpf) return res.sendStatus(403);
+            if (err || foundUser.cpf !== decoded.cpf) return res.sendStatus(403);
             const roles = Object.values(foundUser.roles);
             const accessToken = jwt.sign(
-                { "UserInfo": { "cpf": decoded.cpf, "roles": roles } },
+                { "UserInfo": { "cpf": foundUser.cpf, "roles": roles } },
                 process.env.ACCESS_TOKEN_SECRET,
                 { expiresIn: '3600s' }
             );
